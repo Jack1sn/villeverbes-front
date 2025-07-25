07a1675fbee8f83204f94ea3537f06bf2cbbd4d0
Jacks1n <jacksmg05@gmail.com>
2025-07-25 08:01:37 -0300

    -m "add...."

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  //production: true, // Ambiente de desenvolvimento
- // apiUrl: 'https://villedesverbes.up.railway.app'// URL da API de desenvolvimento
+  production: false, // Ambiente de desenvolvimento
+  apiUrl:'http://localhost:8080'// URL da API de desenvolvimento
 };
